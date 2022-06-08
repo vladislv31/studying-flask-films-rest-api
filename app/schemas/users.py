9aa638719eef07e1865572057cdbf8bac1551ccb
vladislv31 <vladislv.k31@gmail.com>
2022-06-08 13:26:05 +0300

    user info route

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,10 +1,21 @@
 from pydantic import BaseModel
 
+from app.schemas.roles import RoleSchema
 
-class UserSchema(BaseModel):
+
+class UserBaseSchema(BaseModel):
     id: int
     username: str
+
+
+class UserSchema(UserBaseSchema):
 
     class Config:
         orm_mode = True
 
+
+class UserWithRoleSchema(UserBaseSchema):
+    role: RoleSchema
+
+    class Config:
+        orm_mode = True
